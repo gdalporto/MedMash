e3180ddf75f3412838ac9d35348afd2e243a5de3
Gabriel Dalporto <dalporto@yahoo.com>
2018-11-02 11:24:06 -0700

    working model now need to streamline code

--- index.js
+++ index.js
@@ -59,11 +59,42 @@ function searchYoutube(term){
 //    console.log(firstVideo.id);
 //    $('.youtubeResults').html(youtubeHTML(firstVideo));
 //}
+
+function yelpHTML (index, item){
+    
+        // Store each business's object in a variable
+        var id = item.id;
+        var alias = item.alias;
+        var phone = item.display_phone;
+        var image = item.image_url;
+        var name = item.name;
+        var rating = item.rating;
+        var reviewcount = item.review_count;
+        var address = item.location.address1;
+        var city = item.location.city;
+        var state = item.location.state;
+        var zipcode = item.location.zip_code;
+        // Append our result into our page
+//                console.log(phone);
+        return `
+        <div id="${id}">
+            <img src="${image}" style="width:200px;height:150px;"><br>
+            We found <b>${name}</b> (${alias})<br>
+            Business ID: ${id}<br> 
+            Located at: ${address} ${city} ${state} ${zipcode}<br>
+            The phone number for this business is: ${phone}<br>
+            This business has a rating of ${rating} with ${reviewcount} reviews.
+        </div>')`;
+};
+
+
+
 function searchYelp(term){
 
     let YelpTerm=term+" doctor";
     let location="chicago";
     let YelpUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+YelpTerm+"&location="+location;
+    let i=0;
 
     $.ajax({
         url: YelpUrl,
@@ -75,34 +106,23 @@ function searchYelp(term){
         success: function(data){
             // Grab the results from the API JSON return
             console.log(data);
-            var totalresults = data.total;
+            let totalResults=data.total;
+            let numDisplay = Math.min(data.businesses.length, 3);
+            console.log(numDisplay);
             // If our results are greater than 0, continue
-            if (totalresults > 0){
+            if (numDisplay > 0){
                 // Display a header on the page with the number of results
-                $('.YelpResults').html('<h5>We discovered ' + totalresults + ' results!</h5>');
+                $('.YelpResults').html(`<div class="yelpHeader">Here are the top ${numDisplay} physicians for ${term}!</div>`);
                 // Itirate through the JSON array of 'businesses' which was returned by the API
-                $.each(data.businesses, function(i, item) {
-                    // Store each business's object in a variable
-                    var id = item.id;
-                    var alias = item.alias;
-                    var phone = item.display_phone;
-                    var image = item.image_url;
-                    var name = item.name;
-                    var rating = item.rating;
-                    var reviewcount = item.review_count;
-                    var address = item.location.address1;
-                    var city = item.location.city;
-                    var state = item.location.state;
-                    var zipcode = item.location.zip_code;
-                    // Append our result into our page
-        //                console.log(phone);
-                    $('.YelpResults').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
-                });
-            } else {
+                $.each(data.businesses, function(index, item){
+                    $('.YelpResults').append(yelpHTML(index,item));
+                }); 
+            }
+                else {
                 // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
                         console.log("failedtomatch");
                 //           $('#results').append('<h5>We discovered no results!</h5>');
-            }
+            };
         }
     });      
 
